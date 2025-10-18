24e2123c65f68c215cdf9904e2ff8b7466332464
Hadayetullah <hadayetullah30@gmail.com>
2025-10-18 21:01:40 +0600

    Complete partially chapter-9 (Till - Adding loading skeletons).

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 type Props = {}
 
-function page({}: Props) {
+async function page({}: Props) {
+  // console.log('Invoice page starting...');
+  // await new Promise((resolve) => setTimeout(resolve, 4000));
+  // console.log('Invoices page ends after 4 seconds.');
+
   return (
     <div>Invoices page</div>
   )
