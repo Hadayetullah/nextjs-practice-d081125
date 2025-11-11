69e723ce6a2afddad21e9e966c4f468b4e1c65f3
Hadayetullah <hadayetullah30@gmail.com>
2025-11-11 21:08:04 +0600

    Understand how to groups of routes configure, and work with callbacks authorzed method, why does not redirect loop happen while redirect to login page according to the matcher.

--- auth.config.ts
+++ auth.config.ts
@@ -7,7 +7,7 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       // console.log("Auth : ", auth);
-      console.log("nextUrl : ", nextUrl);
+      // console.log("nextUrl : ", nextUrl);
       const isLoggedIn = !!auth?.user;
       console.log("isLoggedIn : ", isLoggedIn);
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
@@ -22,3 +22,46 @@ export const authConfig = {
   },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
+
+
+
+
+{/* For multiple protected routes */}
+// import type { NextAuthConfig } from "next-auth";
+
+// export const authConfig = {
+//   pages: {
+//     signIn: "/login",
+//   },
+//   callbacks: {
+//     authorized({ auth, request: { nextUrl } }) {
+//       const isLoggedIn = !!auth?.user;
+
+//       // Define protected routes
+//       const protectedRoutes = [
+//         "/dashboard",
+//         "/settings",
+//         "/profile",
+//         "/admin"
+//       ];
+
+//       // Check if current path starts with any protected route
+//       const isProtected = protectedRoutes.some((path) =>
+//         nextUrl.pathname.startsWith(path)
+//       );
+
+//       // If route is protected and user not logged in → redirect
+//       if (isProtected && !isLoggedIn) return false;
+
+//       // If logged in and tries to access public routes like /login → redirect to dashboard
+//       if (isLoggedIn && ["/", "/login"].includes(nextUrl.pathname)) {
+//         return Response.redirect(new URL("/dashboard", nextUrl));
+//       }
+
+//       // Allow all other routes
+//       return true;
+//     },
+//   },
+//   providers: [],
+// } satisfies NextAuthConfig;
+
--- middleware.ts
+++ middleware.ts
@@ -7,3 +7,22 @@ export const config = {
   // https://nextjs.org/docs/app/api-reference/file-conventions/proxy#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
+
+
+
+// For protected groups of routes
+// export const config = {
+//   matcher: ["/(protected)(.*)", "/(admin)(.*)"],
+// };
+
+// Project structure example for the protected groups of routes:
+// app/
+//  ├─ (marketing)/
+//  │   ├─ page.tsx         // Public
+//  ├─ (auth)/
+//  │   ├─ login/
+//  ├─ (protected)/
+//  │   ├─ dashboard/
+//  │   ├─ settings/
+//  │   ├─ profile/
+
